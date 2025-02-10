0d8493edbd58e08d931e3a9e3dd2d8fe7b68beb7
diego <diegoan1624@gmail.com>
2025-02-10 04:39:54 -0500

    cambios

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,47 +1,43 @@
-// MainContent.js
 import React from 'react';
 import styled from 'styled-components';
 import fondo from './imagenes/traveling.jpg';
 import phoneIcon from './imagenes/phone.png';
 import emailIcon from './imagenes/email.png';
 import instagramIcon from './imagenes/instagram.png';
-import mapIcon from './imagenes/map.png'; // Asegúrate de que la ruta sea correcta
+import mapIcon from './imagenes/map.png';
 
 const MainContentContainer = styled.div`
     background-image: url(${(props) => props.bgImage});
     background-size: cover;
     background-position: center;
+    background-repeat: no-repeat;
     height: 100vh;
     width: 100vw;
     display: flex;
     justify-content: center;
-    align-items: flex-end; /* Alinea el contenido en la parte inferior */
+    align-items: flex-end;
     color: white;
     text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
-    position: relative; 
-    
-    @media (max-width: 1024px) {
-        width: 100vw; /* Full width */
-        background-size: cover; /* Ensure the background covers the area without repeating */
-        padding: 15px;
-    }
+    position: relative;
+    overflow: hidden; /* Evita desbordamientos */
 
     @media (max-width: 750px) {
-        flex-direction: column;
-        background-size: cover;
-        width: 100vw;
+        align-items: center; /* Centra el contenido verticalmente en móviles */
+        justify-content: center; /* Centra el contenido horizontalmente en móviles */
     }
 `;
 
 const IconsContainer = styled.div`
     display: flex;
     justify-content: center;
-    align-items: center; /* Centra los iconos verticalmente */
+    align-items: center;
     gap: 20px;
-    margin-bottom: 20px; /* Añade un margen inferior para separar los iconos del borde */
+    margin-bottom: 20px;
 
     @media (max-width: 750px) {
-        margin-top: 20px;
+        flex-direction: row; /* Mantén los íconos en fila en móviles */
+        gap: 15px; /* Reduce el espacio entre íconos en móviles */
+        margin-bottom: 10px; /* Reduce el margen inferior en móviles */
     }
 `;
 
@@ -49,12 +45,12 @@ const Icon = styled.img`
     width: 40px;
     height: 40px;
     cursor: pointer;
-    transition: transform 0.2s; /* Transición suave para el efecto hover */
-    
+    transition: transform 0.2s;
+
     &:hover {
-        transform: scale(1.1); /* Aumentar el tamaño del ícono al pasar el mouse */
+        transform: scale(1.1);
     }
-        
+
     @media (max-width: 750px) {
         width: 30px;
         height: 30px;
